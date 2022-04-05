e75814184d06e4544faf4963fb1b3847bc671af0
Miriam Di Giandomenico <miriam.digiandomenico@gmail.com>
2022-04-05 10:43:49 +0200

    Aggiunti commenti String

--- src/main/java/com/example/jse/m01/s10/StringIntro.java
+++ src/main/java/com/example/jse/m01/s10/StringIntro.java
@@ -16,15 +16,17 @@ public class StringIntro {
 
         // Create a mutable string, and then change it
         StringBuilder sb = new StringBuilder("Hello");
-        sb.deleteCharAt(4);
-        sb.append(", Tom!");
-
+        sb.deleteCharAt(4);     //essendo modificabile abbiamo cancellato il 4 carattere, ovvero la 0
+        sb.append(", Tom!");    //aggiunge elementi alla stringa
+        
         // Create a String from a StringBuilder
         String u = sb.toString();
         System.out.println(u);
 
-        // Text block
-        String text = """
+        // Text block //possiamo andare a capo con le stringhe, cosa che non si può fare con quelle normali.
+        //possiamo mettere "tom" direttamente dentro il testo, nella stringa normale non si può fare
+        //per farlo nella stringa normale "Hello, \"Tom\""; 
+        String text = """   
                 Non impedir lo suo fatale andare:
                 vuolsi così colà dove si puote
                 ciò che si vuole, e più non dimandare
