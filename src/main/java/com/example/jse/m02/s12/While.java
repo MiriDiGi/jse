2615b6697f6d7352bb0625a9431dc38e8d8ab3ac
Miriam Di Giandomenico <miriam.digiandomenico@gmail.com>
2022-04-06 12:20:00 +0200

    commenti While

--- src/main/java/com/example/jse/m02/s12/While.java
+++ src/main/java/com/example/jse/m02/s12/While.java
@@ -2,23 +2,23 @@ package com.example.jse.m02.s12;
 
 public class While {
     public static void main(String[] args) {
-        int i = 0;     //inizializziamo variabile
-        while (i < args.length) {   //finchè i < dalla lunghezza dell'argomento
+        int i = 0;                    //inizializziamo variabile FUORI DAL LOOP
+        while (i < args.length) {          //finchè i < dalla lunghezza dell'argomento. c'è solo una clausola
             System.out.println("The argument " + i + " is " + args[i]);
 
             // same as above, more verbosely, see also StringBuilder
-            String s = "The argument ";
-            s = s + i;
-            s = s + " is ";
-            s = s + args[i];
-            System.out.println(s);
+//           String s = "The argument ";
+//           s = s + i;
+//           s = s + " is ";                 possiamo anche evitare lo Stringbuilder
+//           s = s + args[i];
+//            System.out.println(s);
 
-            i += 1;
+            i += 1;  //la variabile di loop è l'ultima cosa
 
             // other ways to increase a number (no side effect here!)
             // i++;
             // ++i;
-            // i = i + 1;
+            // i = i + 1;  modo più lungo di scriverlo
         }
 
         System.out.println("done!");
