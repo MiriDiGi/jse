de6a68f3779ceaefa8cf300c63f42dc2ce2b54ab
Miriam Di Giandomenico <miriam.digiandomenico@gmail.com>
2022-04-07 11:34:05 +0200

    Aggiunti commenti

--- src/main/java/com/example/jse/m03/s02/Main.java
+++ src/main/java/com/example/jse/m03/s02/Main.java
@@ -3,20 +3,21 @@ package com.example.jse.m03.s02;
 public class Main {
     public static void main(String[] args) {
         // calling a static method
-        double root = Math.sqrt(121);
-
+        double root = Math.sqrt(121.0);           //metodo sqrt (square root) ha metodi matematici per fare radice quadrata
+                                                  //serve usare un double altrimenti casta
         // calling an instance method
-        System.out.println(root);
-
-        String aString = Simple.aStaticMethod();
-        System.out.println(aString);
+        System.out.println(root);             //gli passa alla println la radice quadrada, quando invochiamo la println il metodo si ferma per printare
+                                                   //println non ritorna niente, stampa solo sulla console
+        
+        String aString = Simple.aStaticMethod(); //per richiamare un metodo statico serve la classe e il nome del metodo
+        System.out.println(aString);             //il metodo aStaticMethod è nel file simple.java di questa slide
 
         Simple reference = new Simple();
 
-        int result = reference.anInstanceMethod(7, 6);
-        System.out.println(result);
+        int result = reference.anInstanceMethod(7, 6);   //dato che ho il reference posso andare a utilizzare due metodi non statici
+        System.out.println(result);                      //passo come argomenti 7 e 6
 
-        reference.anotherInstanceMethod(true);
+        reference.anotherInstanceMethod(true);        //gli passo un booleano perchè si aspetta un booleano
         reference.anotherInstanceMethod(false);
-    }
+    }                                                   //il metodo main ritorna alla java virtural machine. vuol dire che il programma è finito
 }
--- src/main/java/com/example/jse/m03/s02/Simple.java
+++ src/main/java/com/example/jse/m03/s02/Simple.java
@@ -1,19 +1,23 @@
 package com.example.jse.m03.s02;
 
 public class Simple {
-    static String aStaticMethod() {
+    static String aStaticMethod() {   //metodo statico, non ha bisogno di un oggetto
         return "Hi";
     }
 
-    int anInstanceMethod(int a, int b) {
-        return a * b;
+    int anInstanceMethod(int a, int b) {    //metodi d'istanza, java è un linguaggio fortemente tipizzato ed è ben definita
+        return a * b;                       //metodi d'istanza non hanno lo static davanti
     }
 
-    void anotherInstanceMethod(boolean flag) {
-        if (flag) {
+    void anotherInstanceMethod(boolean flag) {      //dovrò passargli un booleano
+        if (flag) {                              // if (flags) è equivalente a dire  if (flags == true) 
             System.out.println("Hello");
-        } else {
+        } else {                                  //questo metodo non ritorna niente perchè è void, ritorna solo il controllo
             System.out.println("Goodbye");
         }
+    }
+    void servoAQualcosa () {
+    	//metodo che non fa niente e non ritorna niente, solitamente è un segnaposto
+    	// gli diamo un nome ma lo lasciamo vuoto per riempirlo poi
     }
 }
